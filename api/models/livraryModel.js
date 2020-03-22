4e973a66c80cee87512d92ccc8d5a23b6ec5e299
NRodriguezAlvar <nieves.rodriguez_alvar@edu.devinci.fr>
2020-03-22 15:22:51 +0100

    movieSchema

---
+++ api/models/livraryModel.js
@@ -0,0 +1,47 @@
+'use strict';
+
+//Modules
+var mongoose  = require('mongoose');
+//Schema
+var Schema    = mongoose.Schema;
+var movieSchema = new Schema({
+    actorID: {
+		type: String,
+		required: true
+	},
+	link: {
+		type: String
+	},
+	id: {
+		type: String
+	},
+	metascore: {
+		type: Number
+	},
+	poster: {
+		type: String
+	},
+	rating: {
+		type: Number
+	},
+	synopsis: {
+		type: String
+	},
+	title: {
+		type: String
+	},
+	votes: {
+		type: Number
+	},
+	year: {
+		type: Number
+	},
+	date: {
+		type: String
+	},
+	review: {
+		type: String
+	}
+}, { versionKey: false });
+
+module.exports = mongoose.model('Movie', movieSchema);
